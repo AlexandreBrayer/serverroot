adf9d8578eed0eb781fd4eea6a0a77325b08b54e
AlexandreBrayer <nephis.alexandre@gmail.com>
2020-02-22 21:18:31 +0100

    Add files via upload

--- Server.gd
+++ Server.gd
@@ -1,9 +1,7 @@
 extends Node2D
 
 const DEFAULT_PORT = 8910
-const MAX_PEERS    = 10
-var   players      = {}
-var   player_name
+const MAX_PEERS    = 2
 
 func _ready():
 	get_tree().connect("network_peer_connected", self, "_player_connected")
@@ -13,7 +11,6 @@ func _ready():
 	get_tree().connect("server_disconnected", self, "_server_disconnect")
 #10.20.85.189
 func start_server():
-	player_name = 'Server'
 	var host  = NetworkedMultiplayerENet.new()
 	
 	var err = host.create_server(DEFAULT_PORT, MAX_PEERS)
@@ -21,18 +18,16 @@ func start_server():
 	if (err!=OK):
 		join_server()
 		return
-		
 	get_tree().set_network_peer(host)
 	
 func join_server():
-	player_name = 'Client'
 	var host  = NetworkedMultiplayerENet.new()
 	
 	host.create_client($txtIP.text, int($txtPort.text))
 	get_tree().set_network_peer(host)
 
-func _player_connected(_id):
-	hide()
+func _player_connected():
+	print("TOTAL PLAYERS : " + str(get_tree().get_network_connected_peers().size()+1))
 
 func _player_disconnected():
 	print("Player disconnected")
@@ -47,7 +42,7 @@ func _connected_fail():
 	pass
 
 func _server_disconnect():
-	print("Disconected")
+	print("Disconnected")
 
 func _on_btnHost_pressed():
 	start_server()
@@ -55,6 +50,17 @@ func _on_btnHost_pressed():
 func _on_btnJoin_pressed():
 	print("Join request")
 	join_server()
+
+func spawn_player(id):
+	print(id)
+	
+	var player_scene = load("res://testplayer.tscn")
+	var player = player_scene.instance()
+
+	if id == get_tree().get_network_unique_id():
+		player.set_network_master(id)
+	
+	get_parent().add_child(player)
 
 remote func salut(id):
 	print(id)
